54f40d81ab239c7b9e0cc0860b6fd0687c593128
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-11-12 22:15:07 +0600

    hotel count end point

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -39,6 +39,7 @@ export const getHotel= async (req,res,next)=>{
         next()
     }
 }
+
 export const getHotels= async (req,res,next)=>{
     try {
         const  hotels= await Hotel.find()
@@ -47,3 +48,23 @@ export const getHotels= async (req,res,next)=>{
         next()
     }
 }
+export const  countByCity= async (req,res,next)=>{
+    const cities= req.query.cities.split(",")
+    try {
+        const list = await Promise.all(cities.map(city =>{
+            return Hotel.countDocuments({city:city})
+        }))
+        res.status(200).json(list)
+    } catch (err) {
+        next()
+    }
+}
+
+// export const  countByType= async (req,res,next)=>{
+//     try {
+//         const  hotels= await Hotel.find()
+//         res.status(200).json(hotels)
+//     } catch (err) {
+//         next()
+//     }
+// }
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
 import Hotel from '../models/Hotel.js'
 import { creteError } from '../utils/error.js'
 import { veriftAdmin } from '../utils/verifiyToken.js'
@@ -19,5 +19,8 @@ router.get("/:id", getHotel)
 //GET ALL HOTELS
 
 router.get("/",getHotels)
+
+router.get("/countByCity",countByCity)
+// // router.get("/countByType",countByType)
 
 export default router
